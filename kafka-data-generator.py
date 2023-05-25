688caed42291753d5fa2142379681f2133f1a5b9
Brajesh Sharma <srbrajeshsharma@gmail.com>
2023-05-25 21:09:26 +0530

    Adding files

---
+++ kafka-data-generator.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import random
+import time
+
+# Generate a unique identifier for each click event.
+def generate_row_key():
+  return str(random.randint(1, 1000000))
+
+# Generate the click data for a click event.
+def generate_click_data():
+  user_id = random.randint(1, 1000000)
+  timestamp = time.time()
+  url = "https://www.example.com/"
+  return {
+    "user_id": user_id,
+    "timestamp": timestamp,
+    "url": url
+  }
+
+# Generate the geo data for a click event.
+def generate_geo_data():
+  country = random.choice(["United States", "United Kingdom", "Canada"])
+  city = random.choice(["New York City", "London", "Toronto"])
+  return {
+    "country": country,
+    "city": city
+  }
+
+# Generate the user agent data for a click event.
+def generate_user_agent_data():
+  browser = random.choice(["Chrome", "Firefox", "Safari"])
+  operating_system = random.choice(["Windows", "Mac", "Linux"])
+  device = random.choice(["Desktop", "Mobile", "Tablet"])
+  return {
+    "browser": browser,
+    "operating_system": operating_system,
+    "device": device
+  }
+
+# Generate a clickstream event.
+def generate_clickstream_event():
+  row_key = generate_row_key()
+  click_data = generate_click_data()
+  geo_data = generate_geo_data()
+  user_agent_data = generate_user_agent_data()
+  return {
+    "row_key": row_key,
+    "click_data": click_data,
+    "geo_data": geo_data,
+    "user_agent_data": user_agent_data
+  }
+
+# Generate a batch of clickstream events.
+def generate_batch_of_clickstream_events(batch_size):
+  events = []
+  for _ in range(batch_size):
+    events.append(generate_clickstream_event())
+  return events
+
+
+# In[12]:
+
+
+from pykafka import KafkaClient
+import threading
+
+KAFKA_HOST = "10.0.0.16:9092" # Or the address you want
+
+client = KafkaClient(hosts = KAFKA_HOST)
+topic = client.topics["clickstream"]
+
+events = generate_batch_of_clickstream_events(100)
+
+with topic.get_sync_producer() as producer:
+    for event in events:
+        encoded_message = str(event).encode("utf-8")
+        producer.produce(encoded_message)
+
+
+# In[ ]:
+
+
+
+
---
+++ writing_into_elastic.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# import os
+
+# spark_version = '3.1.2'
+# os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:{}'.format(spark_version)
+
+
+# In[2]:
+
+
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+
+# Create a Spark session.
+spark = SparkSession.builder\
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.2")\
+        .config('spark.driver.bindAddress','10.0.0.16').appName("Clickstream Data Processing").getOrCreate()
+
+# Read the clickstream data from Kafka.
+df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "10.0.0.16:9092").option("subscribe", "clickstream").load()
+
+# Aggregate the clickstream data by URL and country.
+agg_df = df.groupBy("url", "country").agg(
+    count("*").alias("clicks"),
+    countDistinct("user_id").alias("unique_users"),
+    avg(unix_timestamp("timestamp") - unix_timestamp("click_data.timestamp")).alias("average_time_spent")
+)
+
+# Write the aggregated data to Elasticsearch.
+agg_df.writeStream.format("org.elasticsearch.spark.sql").mode("append").option("es.nodes", "localhost:8200").option("es.index", "clickstream").start()
+
+# Start the Spark streaming job.
+spark.streams.awaitTermination()
+
+
+# In[ ]:
+
+
+
+
